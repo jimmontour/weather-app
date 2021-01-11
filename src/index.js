51b72f21a756df28dda93c3749ee5290263c8929
Jim <jimmontour@gmail.com>
2021-01-10 20:06:48 -0500

    Hard reset

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './main.css';
+
+const cityName = 'buffalo';
+
+async function getWeather() {
+   try {
+      const weatherRequest = await fetch(
+         `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=9fea2eee139f9e0daabf0cf1f712a5fa`,
+         { mode: 'cors' }
+      );
+      const weatherData = await weatherRequest.json();
+      return weatherData;
+   } catch (err) {
+      console.log(err);
+   }
