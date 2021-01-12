650a5d42dea82dc3f49da43b0d298b22adf8c52e
Jim <jimmontour@gmail.com>
2021-01-11 20:54:31 -0500

    Clean up styles

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import './main.css';
 import getWeather, { cityInfo } from './app/getWeather';
-import searchCity from './app/searchCity';
 
-// Variables:
-let cityName = '';
+getWeather();
 
-// CACHE DOM:
-const submit = document.querySelector('.weather-search__btn');
-const input = document.querySelector('.weather-search__input');
-
-// Event Delegation:
-submit.addEventListener('click', () => {
-  cityName = input.value.toLowerCase();
-  getWeather(`${cityName}`);
-});
-
-// Run:
+console.log('main page', cityInfo);
